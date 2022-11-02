e96a07ae40b7fe725050f2fda9705ed51f7b4e1e
Александр Халезин Дома <iskandersunset@gmail.com>
2022-11-02 21:11:01 +0500

    Добавили проверку наличия собаки в почте

--- tests/test_something.py
+++ tests/test_something.py
@@ -2,6 +2,7 @@ import requests
 
 from configuration import SERVICE_URL
 from src.base_classes.response import Response
+from src.scheme.user import User
 
 # resp = requests.get(SERVICE_URL)
 #
@@ -11,7 +12,8 @@ from src.base_classes.response import Response
 def test_getting_users_list():
     response = requests.get(SERVICE_URL)
     test_object = Response(response)
-    test_object.assert_status_code(200)
+    test_object.assert_status_code(200).validate(User)
+    print(response)
 
 # {
 #     "meta": {
