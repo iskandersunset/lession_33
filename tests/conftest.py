cbf9f734ab123fa2aa8a53eb77e5c853522ebc85
Александр Халезин <iskandersunset@gmail.com>
2022-11-07 16:05:19 +0500

    Пример get_users

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,13 @@
 import pytest
+import requests
+
+from configuration import SERVICE_URL
+
+
+@pytest.fixture
+def get_users():
+    response = requests.get(SERVICE_URL)
+    return response
 
 
 @pytest.fixture
